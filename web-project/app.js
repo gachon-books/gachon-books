d6706273f0cd2eef8d8c107d0cb34d6e7e0e4f8f
youseokhwan <tjrghks115@naver.com>
2019-10-28 19:17:48 +0900

    login.ejs, login.js, signup.ejs, signup.js 생성 및 연결, css: webfonts 추가하여 오류 해결

--- web-project/app.js
+++ web-project/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var loginRouter = require('./routes/login');
+var signupRouter = require('./routes/signup');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use('/bootstrap/css', express.static(__dirname + '/node_modules/bootstrap/di
 
 app.use('/', indexRouter);
 app.use('/login', loginRouter);
+app.use('/signup', signupRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
