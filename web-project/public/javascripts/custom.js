6fe1e8437e4a288f5e129e940e0c6b3bef9d4898
youseokhwan <tjrghks115@naver.com>
2019-11-08 00:56:00 +0900

    전체 놀이시설 목록 가져오는 기능 임시 구현, tooltip 구현

--- web-project/public/javascripts/custom.js
+++ web-project/public/javascripts/custom.js
@@ -1,6 +1,12 @@
-// 지역 카테고리를 선택하면 usage는 안보이도록 설정
 $(function() {
+    // tooltip 초기화
+    $('[data-toggle="tooltip"]').tooltip()
+
+    // 처음 진입하면 카테고리 선택 안내 문구를 출력
+    $('#collapse-usage').collapse('show');
+
+    // 지역 카테고리를 선택하면 usage는 안보이도록 설정
     $('.btn-group').on('click', function() {
-        $('#collapse-usage').hide();
+        $('#closeUsage').trigger('click');
     });
 });
--- web-project/routes/index.js
+++ web-project/routes/index.js
@@ -6,19 +6,21 @@ const cheerio = require('cheerio');
 const session = require('express-session');
 const router = express.Router();
 
-// 우수 놀이시설 정보 가져오기
-let bestFacilities = [];
 let apiKey = 'apiKey'; // 경기데이터드림에서 발급된 apiKey
+
+// 우수 놀이시설 정보
+let bestFacilities = [];
 let bestApiUrl = `https://openapi.gg.go.kr/ExcellenceChildPlayFaciliti?KEY=${apiKey}`;
 
+// 전체 놀이시설 정보
+let facilities = [];
+
+// 공공 DB에서 우수 놀이시설 정보 받아오기
 request.get({ url: bestApiUrl }, function(err, res, body) {
   let $ = cheerio.load(body);
   let arr = $('ExcellenceChildPlayFaciliti').children('row').children('FACLT_NM');
 
-  /*
-    도로명 주소를 우선적으로 가져오되, 없으면 지번 주소를 가져오고
-    그마저도 없으면 대체 텍스트 대입
-  */
+  // 도로명 주소 혹은 지번 주소 가져오기
   let getAddr = function(i) {
     try {
       return arr.prevObject[i].children[17].children[0].data;
@@ -50,6 +52,9 @@ request.get({ url: bestApiUrl }, function(err, res, body) {
 
 router.get('/', function(req, res, next) {
   let sigun_nm = req.query.sigun_nm;
+  let sigun_cd = req.query.sigun_cd;
+  console.log(`sigun_nm: ${sigun_nm}`);
+  console.log(`sigun_cd: ${sigun_cd}`);
 
   // html 태그와 head 태그
   let htmlstream = fs.readFileSync(__dirname + '/../views/htmlhead.ejs', 'utf8');
@@ -69,11 +74,39 @@ router.get('/', function(req, res, next) {
   // 각 지역 놀이시설
   htmlstream += fs.readFileSync(__dirname + '/../views/search.ejs', 'utf8');
 
-  // 시군로고를 클릭했을 경우 list-modal에 놀이시설 목록 출력
-  // {
-  //   ... req.query ...
-    htmlstream += fs.readFileSync(__dirname + '/../views/searchmodal.ejs', 'utf8');
-  // }
+  /*
+    이 부분 동기처리해야함
+    데이터는 잘 불러오는데 새로고침 한 두번 해야됨
+    +) 시군코드 정보 DB화 해서 해야할 듯(일단 시군코드 일일히 넣고 실행)
+  */
+  // 시군로고를 클릭했을 경우 시설의 list와 해당 시설의 info 출력
+  if(sigun_cd != 'undefined') {
+    // 공공 DB에서 전체 놀이시설 정보 받아오기
+    let apiUrl = `https://openapi.gg.go.kr/ChildPlayFacility?KEY=${apiKey}&SIGUN_CD=${sigun_cd}`;
+    request.get({ url: apiUrl }, function(err, res, body) {
+      let $ = cheerio.load(body);
+      let arr = $('ChildPlayFacility').children('row').children('PLAY_FACLT_NM');
+
+      // 도로명 주소 혹은 지번 주소 가져오기
+      // { ... }
+
+      for(let i = 0; i < arr.prevObject.length; i++) {
+        let facility = {
+          'test' : arr.prevObject[i].children[9].children[0].data
+          // 'cityName' : arr.prevObject[i].children[3].children[0].data,   // 시군 이름
+          // 'cityCode' : arr.prevObject[i].children[5].children[0].data,   // 시군 코드
+          // 'name'     : arr.prevObject[i].children[7].children[0].data,   // 놀이시설 이름
+          // 'tel'      : arr.prevObject[i].children[13].children[0].data,  // 전화번호
+          // 'addr'     : getAddr(i),                                       // 주소
+          // 'logt'     : arr.prevObject[i].children[21].children[0].data,  // 경도
+          // 'lat'      : arr.prevObject[i].children[23].children[0].data   // 위도
+        };
+        facilities.push(facility);
+        // console.log(facility);
+      }
+    });
+  }
+  htmlstream += fs.readFileSync(__dirname + '/../views/facilityinfo.ejs', 'utf8');
 
   // 개발자 소개 및 Contact U
   htmlstream += fs.readFileSync(__dirname + '/../views/teamcontact.ejs', 'utf8');
@@ -85,7 +118,8 @@ router.get('/', function(req, res, next) {
   res.end(ejs.render(htmlstream, {
     title: '어린이 놀이시설 정보',
     bestFacilities : bestFacilities,  // 우수 어린이 놀이시설 리스트
-    sigun_nm : sigun_nm
+    sigun_nm : sigun_nm,
+    facilities : facilities // 전체 어린이 놀이시설 리스트
   }));
 });
 
