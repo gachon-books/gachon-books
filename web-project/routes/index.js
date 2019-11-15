7e3d9dca652c6d8795070e9476fc2220d921f17a
youseokhwan <tjrghks115@naver.com>
2019-11-15 18:46:59 +0900

    제목 변경, 배경사진 추가

--- web-project/routes/index.js
+++ web-project/routes/index.js
@@ -72,6 +72,7 @@ router.get('/', function(req, res, next) {
   res.end(ejs.render(htmlstream, {
     title: '어린이 놀이시설 정보',
     bestFacilities : bestFacilities,  // 우수 어린이 놀이시설 리스트
+    icons : ["far fa-laugh-wink", "fas fa-child", "fas fa-gifts"], // 우수 놀이시설 리스트 아이콘
     uid: req.session.uid,
     name: req.session.name,
     location: req.session.address
--- web-project/schemas/database.js
+++ web-project/schemas/database.js
@@ -29,5 +29,4 @@ module.exports = () => {
     });
 
     require('./database');
-    // require('./sigun');
 };
